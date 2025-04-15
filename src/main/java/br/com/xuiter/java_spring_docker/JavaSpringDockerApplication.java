33e0992f24f2a5a669c92212398dedba5373c7c1
[CI&T] André Correia <andreoc@ciandt.com>
2025-04-14 22:06:21 -0300

    Criando estrutura basica do projeto

---
+++ src/main/java/br/com/xuiter/java_spring_docker/JavaSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.xuiter.java_spring_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/xuiter/java_spring_docker/JavaSpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xuiter.java_spring_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
